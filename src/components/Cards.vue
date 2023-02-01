a440f4670b3c1a0d40fb1ba22a91ed17d35e08d2
antunespk <antunes.patrick@hotmail.com>
2023-01-31 22:48:07 -0300

    erro img card

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -5,30 +5,26 @@ import { RouterLink } from 'vue-router'
     <section class="cards p-5">
         <div class="d-flex  justify-content-around">
 
-            <div class="card m-2" style="width: 15rem;"> <img
-                    src="../assets/img/WhatsApp Image 2022-10-22 at 20.30.05.jpeg" class="card-img-top" alt="..."
-                    width="150">
+            <div class="card m-2" style="width: 15rem;"> <img src="../assets/img/card1.jpeg" class="card-img-top"
+                    alt="..." width="150">
                 <div class="card-body">
                     <p class="card-text">Os melhores profissionais.</p>
                 </div>
             </div>
-            <div class="card m-2" style="width: 15rem;"> <img
-                    src="../assets/img/WhatsApp Image 2022-10-22 at 20.33.09 (1).jpeg" class="card-img-top" alt="..."
-                    width="150">
+            <div class="card m-2" style="width: 15rem;"> <img src="../assets/img/card2.jpeg" class="card-img-top"
+                    alt="..." width="150">
                 <div class="card-body">
                     <p class="card-text">Diversos serviços.</p>
                 </div>
             </div>
-            <div class="card m-2" style="width: 15rem;"> <img
-                    src="../assets/img/WhatsApp Image 2022-10-22 at 20.33.10.jpeg" class="card-img-top" alt="..."
-                    width="150">
+            <div class="card m-2" style="width: 15rem;"> <img src="../assets/img/card3.jpeg" class="card-img-top"
+                    alt="..." width="150">
                 <div class="card-body">
                     <p class="card-text">Tudo oq você procura em estética.</p>
                 </div>
             </div>
-            <div class="card m-2" style="width: 15rem;"> <img
-                    src="../assets/img/WhatsApp Image 2022-10-22 at 20.30.02.jpeg" class="card-img-top" alt="..."
-                    width="150">
+            <div class="card m-2" style="width: 15rem;"> <img src="../assets/img/card4.jpeg" class="card-img-top"
+                    alt="..." width="150">
                 <div class="card-body">
                     <p class="card-text">Faça seu cadastro e inicie sua busca.</p>
                 </div>
