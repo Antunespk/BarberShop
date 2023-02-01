bd34359172edd676d95c5d0a298291226298c377
antunespk <antunes.patrick@hotmail.com>
2023-02-01 19:02:16 -0300

    config

---
+++ src/service/config.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const configure = {
+    local: import.meta.env.VITE_APIURL // Desenvolvimento
+};
+
+const http = axios.create({
+    baseURL: configure.local,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json;charset=utf-8',
+    }
+});
+
+http.interceptors.request.use(
+    function (config) {
+        const usuario = JSON.parse(sessionStorage.getItem('usuario'));
+        config.headers.Authorization = (usuario != null && usuario.token != null) ? "Bearer " + usuario.token : '';
+        return config;
+    }
+);
+
+export default http;
