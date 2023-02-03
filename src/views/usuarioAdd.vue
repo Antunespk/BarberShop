8323c872f1129986f8a37103386dcd8c7a41dbba
antunespk <antunes.patrick@hotmail.com>
2023-02-03 20:38:38 -0300

    att

--- src/views/usuarioAdd.vue
+++ src/views/usuarioAdd.vue
@@ -1,6 +1,6 @@
 <script>
 import { Usuario } from '../models/usuario';
-import usuarioService from '../models/servicos';
+import usuarioService from '../service/UsuarioService';
 
 var usuario = new Usuario();
 
@@ -235,7 +235,7 @@ main {
 }
 
 .input-box input:hover {
-background-color: rgb(189, 189, 189);
+    background-color: rgb(189, 189, 189);
 }
 
 .input-box input:focus-visible {
